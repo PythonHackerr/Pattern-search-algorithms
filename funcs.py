358e0f6126069b262a3552038d2175801d8b8609
agamalii <andrii.gamalii.stud@pw.edu.pl>
2022-05-16 08:41:08 +0200

    added naive and cmp algos

--- funcs.py
+++ funcs.py
@@ -1,13 +1,69 @@
 def naive(pattern, text):
     list_of_occurs = []
-    pass
-
+    i = 0
+    j = 0
+    k = 0
+    last = len(text) - len(pattern)
+    while i <= last:
+        end = i + len(pattern) - 1
+        while j <= end:
+            if not pattern[k] == text[j]:
+                break
+            if j == end:
+                list_of_occurs.append(i)
+            k += 1
+            j += 1
+        k = 0
+        i += 1
+        j = i
+    return list_of_occurs
 
 def kmp(pattern, text):
     list_of_occurs = []
-    pass
+    def p(pattern):
+        length = len(pattern)
+        arr = [0] * length
+        i = 0
+        j = 1
+        while j < length:
+            if pattern[i] == pattern[j]:
+                arr[j] = i + 1
+                i += 1
+                j += 1
+            elif i != 0:
+                i = arr[i - 1]
+            else:
+                j += 1
+        return arr
+
+    if pattern and text:
+        prefix = p(pattern)
+        n = 0
+        m = 0
+        while n < len(text):
+            if text[n] == pattern[m]:
+                n += 1
+                m += 1
+            elif m != 0:
+                m = prefix[m - 1]
+            else:
+                n += 1
+            if m == len(pattern):
+                list_of_occurs.append(n - m)
+                m = prefix[m - 1]
+    return list_of_occurs
 
 
 def kr(pattern, text):
     list_of_occurs = []
     pass
+
+
+# pat = 'aiai'
+# text = 'aiaisdi, what is aiaiaisdi? aisdaisdii'
+# pat = 'b'
+# text = 'absbab'
+# print(naive(pat, text))
+# print(kmp(pat, text))
+
+# kmp("abraabra", "asdasdasdasd")
--- main.py
+++ main.py
@@ -3,33 +3,46 @@ import tools
 import sys
 
 
-def collect_stats(func, words, steps=10, values_step=100, repeats=1):
+def collect_stats(func, words, text, steps=10, values_step=10, repeats=1):
     measures = {}
     for i in range(steps+1):
         nvalues = i*values_step
         selected_words = words[:nvalues]
         measures[nvalues] = tools.measure_time(
-            lambda: func(selected_words), repeats)
+            lambda: func(selected_words, text), repeats)
     return measures
 
 
-def find_words(find_func, words):
+def find_words(find_func, words, text):
+    i = 1
     for word in words:
-        find_func(word)
+        if len(word) == 1:
+            i += 1
+            continue
+        find_func(word, text)
+    # print(i)
 
 
 def main():
     sys.setrecursionlimit(10000)
-    words = tools.get_words_list(1500, 'pan-tadeusz.txt')
-    stats_naive = collect_stats(lambda: find_words(naive), words, repeats=5)
-    stats_kmp = collect_stats(lambda: find_words(kmp), words, repeats=5)
-    stats_kr = collect_stats(lambda: find_words(kr), words, repeats=5)
+    text = tools.get_file_content('pan-tadeusz.txt')
+    words = tools.get_words_list(1000, 'pan-tadeusz.txt')
+    # print("naive start")
+    # stats_naive = collect_stats(lambda words, text: find_words(naive, words, text), words, text, repeats=1)
+    # print("naive end")
+    # print("kmp started")
+    # stats_kmp = collect_stats(lambda words, text: find_words(kmp, words, text), words, text, repeats=1)
+    # print("kmp ended")
+    # print("kr started")
+    # stats_kr = collect_stats(lambda words, text: find_words(kr, words, text), words, text, repeats=1)
+    # print("kr ended")
+    # stats_kr = collect_stats(lambda words, text: find_words(kr, words, text), words, text, repeats=1)
     data = {
-        'naive': stats_naive,
-        'kmp': stats_kmp,
-        'kr': stats_kr
+        # 'naive': stats_naive,
+        # 'kmpfind': stats_kmp,
+        # 'kr': stats_kr
     }
-    tools.draw_multiple('Find pattern', 'Words, n', 'Time, s', data, 'find_pattern')
+    tools.draw_multiple('Find pattern', 'Words, n', 'Time, s', data, 'find_pattern_optional2')
 
 
 if __name__ == "__main__":
